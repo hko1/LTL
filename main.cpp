05cbb4c097d05e8808e3326cae5a31dac581542b
hko <schuessler089@gmail.com>
2018-04-28 19:54:45 +0200

    inizialcommit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Bug Buggy
+ *
+ * Created on 28. April 2018, 18:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
