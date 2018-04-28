8dd011e3008f47f7e0e2690b2f477db6772403c3
Daniel Beßler <danielb@cs.uni-bremen.de>
2018-04-28 22:55:40 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <SDL_image.h>
 
 #include <stdlib.h> //rand()
+#include <string> //rand()
 
 static bool quitting = false;
 static float r = 0.0f;
@@ -14,6 +15,61 @@ static SDL_GLContext gl_context;
 static GLuint TextureID = 0;
 static int width = 1280;
 static int height = 720;
+
+GLuint loadImage(const std::string &imgPath) {
+    GLuint textureId = 0;
+    SDL_Surface* Surface = IMG_Load(imgPath.c_str());
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    int Mode = GL_RGB;
+    if(Surface->format->BytesPerPixel == 4) {
+        Mode = GL_RGBA;
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, Mode,
+            Surface->w, Surface->h, 0,
+            Mode, GL_UNSIGNED_BYTE, Surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    return textureId;
+}
+
+    void drawImage(GLuint id) {
+        glBindTexture( GL_TEXTURE_2D, id );
+        glBegin(GL_QUADS);
+        // vertex 1
+        glTexCoord2f(0.0, 1.0);
+        glVertex3f(-1.0f, -1.0f, 0.0f);
+        // vertex 2
+        glTexCoord2f(1.0, 1.0);
+        glVertex3f( 1.0f, -1.0f, 0.0f);
+        // vertex 3
+        glTexCoord2f(1.0, 0.0);
+        glVertex3f( 1.0f,  1.0f, 0.0f);
+        // vertex 4
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(-1.0f,  1.0f, 0.0f);
+        glEnd();
+    }
+
+    void pushProjection() {
+        // setup projection matrix
+        glMatrixMode(GL_PROJECTION);
+        glPushMatrix();
+        glLoadIdentity();
+        // setup model view matrix
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glLoadIdentity();
+    }
+
+    void popProjection() {
+        // reset projection matrix
+        glPopMatrix();
+        glMatrixMode(GL_PROJECTION);
+        // reset model view matrix
+        glPopMatrix();
+        glMatrixMode(GL_MODELVIEW);
+    }
 
     void render() {
 
@@ -24,29 +80,10 @@ static int height = 720;
             glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
             glClear( GL_COLOR_BUFFER_BIT );
             
-            glMatrixMode(GL_PROJECTION);
-            glPushMatrix();
-            glLoadIdentity();
-            glMatrixMode(GL_MODELVIEW);
-            glPushMatrix();
-            glLoadIdentity();
-
-            glBindTexture( GL_TEXTURE_2D, TextureID );
-            glBegin(GL_QUADS);
-                    glTexCoord2f(0.0, 1.0);
-                    glVertex3f(-1.0f, -1.0f, 0.0f);
-                    glTexCoord2f(1.0, 1.0);
-                    glVertex3f( 1.0f, -1.0f, 0.0f);
-                    glTexCoord2f(1.0, 0.0);
-                    glVertex3f( 1.0f,  1.0f, 0.0f);
-                    glTexCoord2f(0.0, 0.0);
-                    glVertex3f(-1.0f,  1.0f, 0.0f);
-            glEnd();
-
-            glPopMatrix();
-            glMatrixMode(GL_PROJECTION);
-            glPopMatrix();
-            glMatrixMode(GL_MODELVIEW);
+            // draw the background
+            pushProjection();
+            drawImage(TextureID);
+            popProjection();
 
         SDL_GL_SwapWindow(window);
 
@@ -76,19 +113,8 @@ int main(int argc, char *argv[]) {
 
     SDL_AddEventWatch(watch, NULL);
     
-    SDL_Surface* Surface = IMG_Load("data/z_bg_hubble_35.png");
+    TextureID = loadImage("data/z_bg_hubble_35.png");
     
-    glGenTextures(1, &TextureID);
-    glBindTexture(GL_TEXTURE_2D, TextureID);
-    int Mode = GL_RGB;
-    if(Surface->format->BytesPerPixel == 4) {
-        Mode = GL_RGBA;
-    }
-    glTexImage2D(GL_TEXTURE_2D, 0, Mode,
-            Surface->w, Surface->h, 0,
-            Mode, GL_UNSIGNED_BYTE, Surface->pixels);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glEnable(GL_TEXTURE_2D);
 
     while(!quitting) {
